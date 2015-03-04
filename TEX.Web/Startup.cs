f77039db0bc221f7d793d26b132e7283a16feadb
arunkumar_mcsd <arunkumar_mcsd@yahoo.co.in>
2015-03-04 15:17:02 +0530

    https://github.com/arunkumarmcsd/TexnicAX.git

---
+++ TEX.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEX.Web.Startup))]
+namespace TEX.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
